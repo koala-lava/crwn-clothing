92330ecac99ba85d84050516cf516987044034e5
koala-lava <shgr11111@yandex.ru>
2022-09-27 15:51:38 +0300

    adds sign up, form input and button components

---
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -0,0 +1,112 @@
+import { useState } from 'react'
+
+import {
+  createAuthUserWithEmailAndPassword,
+  createUserDocumentFromAuth,
+} from '../../utils/firebase/firebase.utils'
+
+import FormInput from '../form-input/form-input.component'
+import Button from '../button/button.component'
+
+import './sign-up-form.styles.scss'
+
+const defaultFormFields = {
+  displayName: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+}
+
+const SignUpForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields)
+  const { displayName, email, password, confirmPassword } = formFields
+
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields)
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+
+    const confirmFormInput = event.target.querySelector(
+      '[name=confirmPassword]'
+    )
+
+    if (password !== confirmPassword) {
+      confirmFormInput.setCustomValidity('Passwords do not match')
+      return
+    }
+
+    try {
+      const { user } = await createAuthUserWithEmailAndPassword(email, password)
+
+      await createUserDocumentFromAuth(user, { displayName })
+      resetFormFields()
+    } catch (error) {
+      if (error.code === 'auth/email-already-in-use') {
+        alert('Cannot create user, email already in use')
+      } else {
+        console.log('user creation encountered an error', error.message)
+      }
+    }
+  }
+
+  const handleChange = (event) => {
+    const { name, value } = event.target
+
+    setFormFields({ ...formFields, [name]: value })
+  }
+
+  return (
+    <div>
+      <h2>Don't have an account</h2>
+      <span>Sign up with your email and password</span>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          label='Display Name'
+          required
+          type='text'
+          minLength={3}
+          maxLength={20}
+          onChange={handleChange}
+          name='displayName'
+          value={displayName}
+        />
+
+        <FormInput
+          label='Email'
+          required
+          type='email'
+          onChange={handleChange}
+          name='email'
+          value={email}
+        />
+
+        <FormInput
+          label='Password'
+          required
+          type='password'
+          pattern='^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{6,128}$'
+          title='length min 5 max 50, at least one uppercase letter, number and any of these characters !@#$%^&*_=+-'
+          onChange={handleChange}
+          name='password'
+          value={password}
+        />
+
+        <FormInput
+          label='Confirm Password'
+          required
+          type='password'
+          pattern='^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{6,128}$'
+          title='length min 5 max 50, at least one uppercase letter, number and any of these characters !@#$%^&*_=+-'
+          onChange={handleChange}
+          name='confirmPassword'
+          value={confirmPassword}
+        />
+        <Button type='submit'>Sign Up</Button>
+      </form>
+    </div>
+  )
+}
+
+export default SignUpForm
